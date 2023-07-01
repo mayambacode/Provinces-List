bef40a4e1eef3a91e8714605fd9552bbf345265d
mayambacode <60010317+mayambacode@users.noreply.github.com>
2023-07-01 17:47:04 -0400

    Will name change be successful?

---
+++ src/app/dialogbox/dialogbox.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject, INJECTOR, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Province } from '../province';
+@Component({
+  selector: 'app-dialogbox',
+  templateUrl: './dialogbox.component.html',
+  styleUrls: ['./dialogbox.component.css']
+})
+export class DialogboxComponent implements OnInit {
+
+  province: Province;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Province) { 
+    this.province =data;
+  }
+  // will the name change work?
+  ngOnInit(): void {
+  }
+
+}
